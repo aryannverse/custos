5986c6e54fa82f4eaf558a9a969c94127a726b4c
Aryan Verma <vermaryan1@gmail.com>
2026-07-15 20:36:53 +0530

    fix(app): fix sys.path for streamlit cloud deployment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 from custos.pipeline import TextToSQLPipeline
 
 st.set_page_config(
